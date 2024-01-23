4604b80a9331d6293e0b3577eb5ed0898f8842d3
Tatyana Maltseva <nucha1405@yandex.ru>
2024-01-23 21:47:03 +0300

    feat: создать модели для бд

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+}).then(() => {
+
+});
+
+app.listen(PORT, () => {
+
+});
