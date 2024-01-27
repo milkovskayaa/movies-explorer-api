c70eff867d41e17c9332e4674634c4a0019b47e2
Tatyana Maltseva <nucha1405@yandex.ru>
2024-01-27 14:37:23 +0300

    feat: настроить линтер

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const NotFoundError = require('./errors/not-found');
 
 const { PORT = 3000 } = process.env;
 
@@ -8,7 +9,21 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
   useNewUrlParser: true,
 }).then(() => {
+});
 
+app.use((req, res, next) => {
+  next(new NotFoundError('Такой страницы не существует'));
+});
+
+app.use((err, req, res) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 });
 
 app.listen(PORT, () => {
