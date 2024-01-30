a62b81a6b05d8a7d07a27d254d218d4c36a1f478
Tatyana Maltseva <nucha1405@yandex.ru>
2024-01-30 16:28:31 +0300

    feat: добавить rate-limiter

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const NotFoundError = require('./errors/not-found');
 const { router } = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { limiter } = require('./utils/limiter');
 
-const { PORT = 3000 } = process.env;
+const {
+  PORT = process.env.PORT || 3000,
+  DB_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb',
+} = process.env;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(helmet());
+app.use(limiter);
 
-mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb').then(() => {});
+mongoose.connect(DB_URL).then(() => {});
 
 app.use(router);
 
