1cc7b1f5ea4644cb0710c75555a58b6fccd05ac7
Tatyana Maltseva <nucha1405@yandex.ru>
2024-01-30 21:15:43 +0300

    hotfixes: исправить ошибки

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const { login, createUser } = require('../controllers/users');
 const { auth } = require('../middlewares/auth');
 const { loginValidate, createUserValidate } = require('../middlewares/validation');
 
-router.use('/users', auth, userRouter);
-router.use('/movies', auth, movieRouter);
 router.post('/signin', loginValidate, login);
 router.post('/signup', createUserValidate, createUser);
+router.use(auth);
+router.use('/users', userRouter);
+router.use('/movies', movieRouter);
 
 module.exports = {
   router,
